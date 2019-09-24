5a4648d192148c49f2663aa1884102d462c59d7c
softprops <d.tangren@gmail.com>
2019-09-24 22:42:45 +0900

    examples

---
+++ examples/demo.rs
@@ -0,0 +1,13 @@
+use source_error::Error;
+
+fn main() {
+    println!(
+        "{}",
+        Error::new(
+            "whoopsie!",
+            "../tests/source.json",
+            include_str!("../tests/source.json").lines(),
+            (3, 2)
+        )
+    )
+}
