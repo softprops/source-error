9707b3ec2db9bc7722d8257e6fe64d96d32624cb
softprops <softprops@users.noreply.github.com>
2019-09-25 10:00:22 +0000

    Automated deployment: Wed Sep 25 10:00:22 UTC 2019 d8d5193980b60eb694365d4e66f8d1743d55e087

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["source_error"] = [{text:"impl&lt;S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"source_error/struct.Error.html\" title=\"struct source_error::Error\">Error</a>&lt;S&gt;",synthetic:false,types:["source_error::Error"]},];
+implementors["source_error"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"source_error/struct.Position.html\" title=\"struct source_error::Position\">Position</a>",synthetic:false,types:["source_error::Position"]},{text:"impl&lt;S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"source_error/struct.Error.html\" title=\"struct source_error::Error\">Error</a>&lt;S&gt;",synthetic:false,types:["source_error::Error"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
